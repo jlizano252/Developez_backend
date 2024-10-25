ab36a8c0ff0e425470b2078070619691e27d2568
Jenhson <lizanovillalobosjenhson@gmail.com>
2024-10-25 08:40:40 -0600

    Adding features

---
+++ alfeje/app/Models/Role.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public $table = 'roles';
+
+    public $fillable = [
+        'name',
+        'guard_name'
+    ];
+
+    protected $casts = [
+        'name' => 'string',
+        'guard_name' => 'string'
+    ];
+
+    public static array $rules = [
+        'name' => 'required'
+    ];
+
+    public function users()
+    {
+        return $this->morphedByMany(User::class, 'model', 'model_has_roles', 'role_id', 'model_id');
+    }
+}
