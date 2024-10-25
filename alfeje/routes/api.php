ae4214e05c3e30ce6785d0392c22d096f84d34f6
Jenhson <lizanovillalobosjenhson@gmail.com>
2024-10-24 18:44:57 -0600

    changes

--- alfeje/routes/api.php
+++ alfeje/routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('oauth/token', [AccessTokenController::class, 'issueToken'])
+    ->middleware('throttle');
+
+Route::middleware('auth:api')->group(function () {
+
+    Route::resource('users', UserAPIController::class)
+        ->except(['create', 'edit']);
 });
